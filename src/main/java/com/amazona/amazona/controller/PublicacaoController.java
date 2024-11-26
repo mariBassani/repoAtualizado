37f6add2b4d14f748139c9f5ef358215198aa1b1
Ana Paula Martins <maribassani2007@gmail.com>
2024-11-25 21:03:43 -0300

    controller

--- src/main/java/com/amazona/amazona/controller/PublicacaoController.java
+++ src/main/java/com/amazona/amazona/controller/PublicacaoController.java
@@ -1,5 +1,33 @@
 package com.amazona.amazona.controller;
 
+@RestController
+@RequestMapping("/publicacoes")
 public class PublicacaoController {
-    
+
+    @Autowired
+    PublicacaoRepository publicacaoRepository;
+
+    @GetMapping
+    public List<Publicacao> getPublicacoes() {
+        return StreamSupport.stream(publicacaoRepository
+                .findAll()
+                .spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public Publicacao getPublicacaoById(@PathVariable Long id) {
+        return publicacaoRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Publicacao createNewPublicacao(@RequestBody Publicacao newPublicacao) {
+        return publicacaoRepository.save(newPublicacao);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePublicacaoById(@PathVariable Long id) {
+        publicacaoRepository.deleteById(id);
+    }
 }
+
